2527505021244aba80d6d473f145e12d79c44c84
Junaid Iqbal <jdi.multan@gmail.com>
2018-04-22 11:45:48 +0500

    Function usage...

--- src/com/lesseon1/lambda/Main.java
+++ src/com/lesseon1/lambda/Main.java
@@ -1,11 +1,25 @@
 package com.lesseon1.lambda;
 
 import java.util.function.BiFunction;
+import java.util.function.Function;
 
 public class Main {
 
 	public static void main(String[] args) {
-		//a simple way to write a lambda expression
+		// a simple way to write a lambda expression
 		BiFunction<Integer, Integer, Integer> sum = (x, y) -> x + y;
+
+		// if the only one argument
+		Function<Integer, Integer> identity = x -> x; // (x) -> x; is valid too
+
+		// without the interface
+		Function<Integer, Integer> mult = (Integer x) -> x * 2;
+
+		// with multiple statements
+		Function<Integer, Integer> adder = (x) -> {
+			x += 5;
+			x += 10;
+			return x;
+		};
 	}
 }
