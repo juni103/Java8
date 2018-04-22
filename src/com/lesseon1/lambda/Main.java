669f305b346de6aa051d04016ba52cc5e407200f
Junaid Iqbal <jdi.multan@gmail.com>
2018-04-22 11:44:20 +0500

    Writing lambda expressions

--- src/com/lesseon1/lambda/Main.java
+++ src/com/lesseon1/lambda/Main.java
@@ -1,5 +1,11 @@
 package com.lesseon1.lambda;
 
+import java.util.function.BiFunction;
+
 public class Main {
 
+	public static void main(String[] args) {
+		//a simple way to write a lambda expression
+		BiFunction<Integer, Integer, Integer> sum = (x, y) -> x + y;
+	}
 }
---
+++ src/com/lesseon1/lambda/lesson1_notes/README.md
@@ -0,0 +1,2 @@
+* <b>BiFunction<T, U, R></b> is an interface representing function with two arguments of types T and U. It returns value of type R.
+* <b>Function<T, R></b> s also a special interface but it has only one argument of type T and returns value of type R.
